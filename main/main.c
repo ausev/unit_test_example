b0f4d39220a2ab4afd217d119726f54798d5564b
Aurimas Sevelevicius <aurimas.sevelevicius@teltonika.lt>
2021-10-14 16:08:32 +0300

    added progress

--- main/main.c
+++ main/main.c
@@ -1,10 +1,10 @@
-#include "example.h"
+// #include "example.h"
 // #include "returnnum1.h"
 // #include "returnnum2.h"
 // #include "returnnum3.h"
 #include "returnint1.h"
-#include "returnint2.h"
-#include "returnint3.h"
+// #include "returnint2.h"
+// #include "returnint3.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -12,13 +12,13 @@
 
 int main(){
     printf ("Hello World!\n");
-    printf ("Average of 10, 20, 30 is %d\n\r", AverageThreeBytes(10,20,30));
+    // printf ("Average of 10, 20, 30 is %d\n\r", AverageThreeBytes(10,20,30));
     // printf ("numreturner1 returns %d\n\r", numreturner1(1));
     // printf ("numreturner2 returns %d\n\r", numreturner2(2));
     // printf ("numreturner3 returns %d\n\r", numreturner3(3));
     printf ("intreturner1 returns %d\n\r", intreturner1(1));
-    printf ("intreturner2 returns %d\n\r", intreturner2(2));
-    printf ("intreturner3 returns %d\n\r", intreturner3(3));
+    // printf ("intreturner2 returns %d\n\r", intreturner2(2));
+    // printf ("intreturner3 returns %d\n\r", intreturner3(3));
 
     return 0;
 }
